1e51649bec8e77228947fb3026df5ab1e7fa1413
MoinMarcell <marcell.dechant@protonmail.com>
2022-11-09 21:03:44 +0100

    Initial commit

---
+++ src/main/java/com/github/moinmarcell/autohaus/AutohausApplication.java
@@ -0,0 +1,13 @@
+package com.github.moinmarcell.autohaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutohausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutohausApplication.class, args);
+	}
+
+}
